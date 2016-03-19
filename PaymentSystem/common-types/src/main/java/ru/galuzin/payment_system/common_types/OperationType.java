821a8d8b65e96f82fdebf640e6a2c98ddb3cb0c5
skallod <galuzin.leonid@gmail.com>
2016-03-19 11:43:45 +0300

    добавил комментарии

--- PaymentSystem/common-types/src/main/java/ru/galuzin/payment_system/common_types/OperationType.java
+++ PaymentSystem/common-types/src/main/java/ru/galuzin/payment_system/common_types/OperationType.java
@@ -4,7 +4,16 @@ package ru.galuzin.payment_system.common_types;
  * Created by LEONID on 14.02.2016.
  */
 public enum OperationType {
+    /**
+     * перевод денег с счета на счет
+     */
     MONEY_ORDER,
+    /**
+     * положить деньги на счет с терминала(банкомата)
+     */
     PUT_MONEY,
+    /**
+     * снять деньги со счета с терминала(банкомата)
+     */
     GET_MONEY
 }
