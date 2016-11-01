74ad938bdacfd5a6a695e85bc2edbbed581e32c1
skallod <galuzin.leonid@gmail.com>
2016-11-01 19:44:03 +0300

    czxv

---
+++ NetBeansProjects/JpaTestHefel/src/java/ru/galuzin/hefel/jpa/Customer.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.galuzin.hefel.jpa;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author galuzin
+ */
+@Entity
+@Table(name = "CUSTOMERS") 
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "CUSTOMER_ID")
+    private Long customerId;
+    
+        private String firstName;
+
+    /**
+     * Get the value of firstName
+     *
+     * @return the value of firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Set the value of firstName
+     *
+     * @param firstName new value of firstName
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    private String lastName;
+
+    /**
+     * Get the value of lastName
+     *
+     * @return the value of lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Set the value of lastName
+     *
+     * @param lastName new value of lastName
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String email;
+
+    /**
+     * Get the value of email
+     *
+     * @return the value of email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @param email new value of email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (customerId != null ? customerId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Customer)) {
+            return false;
+        }
+        Customer other = (Customer) object;
+        if ((this.customerId == null && other.customerId != null) || (this.customerId != null && !this.customerId.equals(other.customerId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ru.galuzin.hefel.jpa.Customer[ id=" + customerId + " ]";
+    }
+    
+}
