5466cf6b8c583bca1dc1e19b9432718e8653346b
galuzin <@galuzin>
2017-12-15 18:24:59 +0300

    up

--- examples1/htmltowikimark/src/main/java/ru/galuzin/App.java
+++ examples1/htmltowikimark/src/main/java/ru/galuzin/App.java
@@ -1,4 +1,5 @@
 package ru.galuzin;
+import com.atlassian.renderer.wysiwyg.converter.DefaultWysiwygConverter;
 
 /**
  * Hello world!
@@ -9,5 +10,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        String htmlString = "This is <em>emphasized</em> and <b>bold</b>";
+        DefaultWysiwygConverter converter = new DefaultWysiwygConverter();
+        String wikiMarkupString = converter.convertXHtmlToWikiMarkup(htmlString);
+        System.out.println("wikiMarkupString = " + wikiMarkupString);
+        //Assert.assertEquals("This is _emphasized_ and *bold*", wikiMarkupString);
     }
 }
