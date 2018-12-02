ea331f7e82ab57f457aea445f5520c258a7ba367
skallod <galuzin.leonid@gmail.com>
2018-12-02 14:11:03 +0300

    md format

---
+++ otus/dz04_my_junit/src/main/java/ru/galuzin/my_junit/tested/B.java
@@ -0,0 +1,10 @@
+package ru.galuzin.my_junit.tested;
+
+import ru.galuzin.my_junit.junit.Test;
+
+public class B {
+    @Test
+    public void test1(){
+        System.out.println("test B");
+    }
+}
